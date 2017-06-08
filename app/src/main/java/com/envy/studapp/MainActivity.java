d3d2077b16fbb6e70ec33cb4e5161a51f7fadf50
Alexandr Devyatkin <booombax@gmail.com>
2017-06-08 16:30:28 +0400

    change MainActivity

--- app/src/main/java/com/envy/studapp/MainActivity.java
+++ app/src/main/java/com/envy/studapp/MainActivity.java
@@ -3,12 +3,16 @@ package com.envy.studapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.envy.studapp.HTTPAPIInterface.DaggerInjectorInterface;
 import com.envy.studapp.HTTPAPIInterface.InjectorInterface;
+import com.envy.studapp.HTTPAPIInterface.StudServiceAPI;
 
 import javax.inject.Inject;
 
 public class MainActivity extends AppCompatActivity {
 
+    @Inject
+    StudServiceAPI studServiceAPI;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +23,6 @@ public class MainActivity extends AppCompatActivity {
         subject.getSchedule();*/
 
         InjectorInterface.inject(this);
+
     }
 }
